4648e9a3a257cf728ae1bed4cd13fb478b2f8c4a
Gustavo Almeida <gustavo.abcdef@hotmail.com>
2024-10-30 22:46:57 +0000

    Add entrypoint.sh file

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+trap "Ok received Exit" HUP INT QUIT TERM
+
+start_rsyslogd() {
+    sed -i "s|\${LOG_FILE_DIR}|$LOG_FILE_DIR|g" /etc/rsyslog.d/kamailio.conf
+    sed -i "s|\${LOG_FILE_DIR}|$LOG_FILE_DIR|g" /etc/logrotate.d/kamailio
+    sed -i "s|\${LOG_FILE_NAME}|$LOG_FILE_NAME|g" /etc/rsyslog.d/kamailio.conf
+    sed -i "s|\${LOG_FILE_NAME}|$LOG_FILE_NAME|g" /etc/logrotate.d/kamailio
+    rsyslogd -n &
+}
+
+case "$1" in
+    shell)
+        exec /bin/bash --login
+        ;;
+    start)
+        start_rsyslogd
+        /usr/sbin/kamailio -DD -P ${TMP_FILE_LOCATION}/kamailio.pid -u $STARTER_KAM_USER -g $STARTER_KAM_GROUP -f $CONFIGS_FOLDER/$CFGFILE -m $SHM_MEMORY -M $PKG_MEMORY
+        ;;
+    *)
+        echo "Executing custom command"
+        exec "$@"
+        ;;
+esac
